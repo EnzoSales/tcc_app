f673eb198645036ae2900182caa11007ff1fb1d6
EnzoSales <enzo.v.sales@gmail.com>
2022-02-27 15:10:08 -0300

    1.5version:TextFormField,Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:easy_debounce/easy_debounce.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,6 +24,49 @@ class LoginPage extends StatelessWidget {
               mainAxisSize: MainAxisSize.max,
               children: [
                 // Text("Pagina de Login"),
+                Padding(
+                 padding: EdgeInsetsDirectional.all(25),
+                  child: TextFormField(
+                    // onChanged: ,
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.person),
+                      labelText: 'Email',
+                    ),
+                    onSaved: (String value) {
+                      // This optional block of code can be used to run
+                      // code when the user saves the form.
+                    },
+                    validator: (String value) {
+                      return (value != null && value.contains('@')) ? 'Do not use the @ char.' : null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.all(25),
+                  child:TextFormField(
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.password),
+                      labelText: 'Senha',
+                    ),
+                    onSaved: (String value) {
+                      // This optional block of code can be used to run
+                      // code when the user saves the form.
+                    },
+                    validator: (String value) {
+                      return (value != null && value.contains('@')) ? 'Do not use the @ char.' : null;
+                    },
+                  )
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.zero,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      primary: Color.fromARGB(255, 25, 8, 175),
+                      backgroundColor: Color.fromARGB(255, 25, 8, 175)
+                    ),
+                    child: Text("Logar"),
+                  ),
+                )
               ],
             ),
           )
