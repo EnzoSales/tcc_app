8375f34a59c8bc4bc0f7e7ba01494924dfc85067
EnzoSales <enzo.v.sales@gmail.com>
2022-02-28 18:40:36 -0300

    1.0version

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,77 @@
+import 'dart:js';
 import 'package:easy_debounce/easy_debounce.dart';
 import 'package:flutter/material.dart';
+import 'package:tcc_app/Home.dart';
 
 void main() {
-  runApp(LoginPage());
+  runApp(Login());
 }
 
-class LoginPage extends StatelessWidget {
+//flutter stateless widget
+ class Login extends StatefulWidget {
+  const Login({ Key key }) : super(key: key);
 
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+
+  void Conect() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Home()),
+      );
+    }
   @override
   Widget build(BuildContext context) {
 
-    // throw UnimplementedError();
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Login"),
         ),
         body: SafeArea(
-          child: Align(
-            alignment: Alignment(0,0) ,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                // Text("Pagina de Login"),
-                Padding(
-                 padding: EdgeInsetsDirectional.all(25),
-                  child: TextFormField(
-                    // onChanged: ,
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.person),
-                      labelText: 'Email',
-                    ),
-                    onSaved: (String value) {
-                      // This optional block of code can be used to run
-                      // code when the user saves the form.
-                    },
-                    validator: (String value) {
-                      return (value != null && value.contains('@')) ? 'Do not use the @ char.' : null;
-                    },
+            child: Align(
+          alignment: Alignment(0, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              // Text("Pagina de Login"),
+              Padding(
+                padding: EdgeInsetsDirectional.all(25),
+                child: TextFormField(
+                  // onChanged: ,
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.person),
+                    labelText: 'Usuario',
                   ),
                 ),
-                Padding(
+              ),
+              Padding(
                   padding: EdgeInsetsDirectional.all(25),
-                  child:TextFormField(
+                  child: TextFormField(
                     decoration: const InputDecoration(
                       icon: Icon(Icons.password),
                       labelText: 'Senha',
                     ),
-                    onSaved: (String value) {
-                      // This optional block of code can be used to run
-                      // code when the user saves the form.
-                    },
-                    validator: (String value) {
-                      return (value != null && value.contains('@')) ? 'Do not use the @ char.' : null;
-                    },
-                  )
+                  )),
+              Padding(
+                padding: EdgeInsetsDirectional.zero,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      primary: Color.fromARGB(255, 6, 248, 19),
+                      backgroundColor: Color.fromARGB(255, 6, 248, 19)),
+                  onPressed: Conect,
+                  child: Text("Logar"),
                 ),
-                Padding(
-                  padding: EdgeInsetsDirectional.zero,
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      primary: Color.fromARGB(255, 25, 8, 175),
-                      backgroundColor: Color.fromARGB(255, 25, 8, 175)
-                    ),
-                    child: Text("Logar"),
-                  ),
-                )
-              ],
-            ),
-          )
-        ),
+              )
+            ],
+          ),
+        )),
       ),
     );
   }
-
-} 
+}
