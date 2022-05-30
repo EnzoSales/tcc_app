ed8bc75347e7df89a14d658cbe5031fab2f3c612
EnzoSales <enzo.v.sales@gmail.com>
2022-05-30 18:14:17 -0300

    1.0EnviarDados

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
+import 'dart:collection';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tcc_app/Home.dart';
 import 'package:tcc_app/loginGoogle.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() => runApp(
   MaterialApp(
@@ -58,6 +59,7 @@ class _LoginState extends State<Login> {
             ),
           );
         });
+        FirebaseFirestore.instance.collection('tccProject-27997').add({'usuario':GoogleSignInAccount});
       },
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
       highlightElevation: 0,
